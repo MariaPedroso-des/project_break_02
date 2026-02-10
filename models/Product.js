1f7206b3c7f7704204f9b806ac97591faca56b0b
MariaPedroso-des <marhiapm@gmail.com>
2026-02-10 13:26:40 +0100

    models/product definido

--- models/Product.js
+++ models/Product.js
@@ -1,13 +1,55 @@
-// Herramientas para operar con MongoDB
+// Herramientas para operar con MongoDB. 
+// Aquí definimos qué es un producto.
 
-// EJEMPLO
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const TaskSchema = new mongoose.Schema({
-//     title: String,
-//     completed: Boolean,
-// }, { timestamps: true });
+const validColors = ['rojo', 'azul', 'verde', 'negro', 'blanco', 'gris']
+const validCategory = ['ropa', 'gorras', 'accesorios', 'vinilos']
+const validSize = ['XS', 'S', 'M', 'L', 'XL']
 
-// const Task = mongoose.model('Task', TaskSchema);
+const ProductSchema = new mongoose.Schema({
+    name: { 
+      type: String, 
+      required: true,
+      minlength: 3,
+      trim: true
+    },
+    description: { 
+      type: String, 
+      required: true,
+      trim: true
+    },
+    image: {                   // Si tengo tiempo, hacer un array con varias imágenes por producto.
+      type: String,
+      trim: true
+    },
+    color: { 
+      type: String, 
+      enum: validColors, 
+      required: true,
+    },
+    category: { 
+      type: String, 
+      enum: validCategory, 
+      required: true,
+    },
+    size: { 
+      type: String,
+      enum: validSize, 
+      required: true 
+    },
+    price: { 
+      type: Number, 
+      required: true, 
+      min: 0.01
+    },
+}, { timestamps: true });
 
-// module.exports = Task;
+const Product = mongoose.model('Product', ProductSchema);
+
+module.exports = Product
+
+// Por si me hacen falta en otro archivo
+module.exports.validColors = validColors;
+module.exports.validCategory = validCategory;
+module.exports.validSize = validSize;
